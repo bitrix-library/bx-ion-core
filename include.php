4dfb615a914752e33d53988721df6b3d55cf2a25
Nick <thebeterdays@gmail.com>
2020-10-15 17:20:37 +0300

    [FIX]

--- include.php
+++ include.php
@@ -1,11 +1,11 @@
 <?php
-require __DIR__.'/vendor/autoload.php';
+require __DIR__ . '/vendor/autoload.php';
 
 use Ion\Ion;
 use Ion\ArrayHelper;
 use Bitrix\Main\Loader;
 
 Loader::registerAutoLoadClasses('ion', array(
-	Ion::class => './classes/Ion.php',
-	ArrayHelper::class => './classes/ArrayHelper.php'
+    '\\' . Ion::class => './classes/Ion.php',
+    '\\' . ArrayHelper::class => './classes/ArrayHelper.php'
 ));
