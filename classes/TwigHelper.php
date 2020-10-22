2b30843a6a899035a52589e5ee7e0755275389a8
amensum <amensum@ya.ru>
2020-10-22 18:09:19 +0300

    [UP] TwigHelper

--- classes/TwigHelper.php
+++ classes/TwigHelper.php
@@ -2,13 +2,10 @@
 
 namespace Ion;
 
-use CBitrixComponent;
 use Twig\Environment;
 use Twig\Error\LoaderError;
-use Twig\Error\RuntimeError;
 use Twig\Error\SyntaxError;
 use Twig\Loader\ArrayLoader;
-use Twig\Loader\FilesystemLoader;
 
 /**
  * Class TwigHelper
@@ -31,30 +28,6 @@ class TwigHelper
 		$loader = new ArrayLoader();
 		$twig = new Environment($loader, $envOptions);
 		$template = $twig->createTemplate($string);
-
-		return $template->render($params);
-	}
-
-	/**
-	 * @param CBitrixComponent $component
-	 * @param array $params
-	 * @param array $envOptions
-	 * @return string
-	 * @throws LoaderError
-	 * @throws RuntimeError
-	 * @throws SyntaxError
-	 */
-	public static function renderBC(CBitrixComponent $component, array $params, array $envOptions = []): string
-	{
-		global $DOCUMENT_ROOT;
-
-		$componentPath = $component->getPath();
-		$templateName = $component->getTemplateName();
-		$templatePath = $DOCUMENT_ROOT . '/' . $componentPath . '/templates/' . $templateName;
-
-		$loader = new FilesystemLoader($templatePath);
-		$twig = new Environment($loader, $envOptions);
-		$template = $twig->load('template.twig');
 
 		return $template->render($params);
 	}
