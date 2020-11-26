ce0c7013020720a239b6927a84952d233960befc
amensum <amensum@ya.ru>
2020-11-26 09:50:33 +0300

    [UP] Global Domains

--- include.php
+++ include.php
@@ -19,3 +19,11 @@ Loader::registerAutoLoadClasses('ion', array(
 	'\\' . ComponentInterface::class => './classes/ComponentInterface.php',
 	'\\' . TwigComponent::class => './classes/TwigComponent.php',
 ));
+
+$SERVER_NAME_ARR = explode(".", strtoupper($_SERVER["SERVER_NAME"]));
+$SERVER_NAME_ARR_REV = array_reverse($SERVER_NAME_ARR);
+$GLOBALS["DOMAIN_FIRST"] = $SERVER_NAME_ARR_REV[0];
+$GLOBALS["DOMAIN_SECOND"] = $SERVER_NAME_ARR_REV[1];
+$GLOBALS["DOMAIN_THIRD"] = $SERVER_NAME_ARR_REV[2];
+$GLOBALS["DOMAIN_FOURTH"] = $SERVER_NAME_ARR_REV[3];
+$GLOBALS["DOMAIN_FIFTH"] = $SERVER_NAME_ARR_REV[4];
