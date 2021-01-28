f88b7b6d8285be71bc9bbafbaeb0c449e81a8963
amensum <amensum@ya.ru>
2021-01-29 02:22:43 +0300

    [UP] Renderers

--- classes/ReactRenderer.php
+++ classes/ReactRenderer.php
@@ -10,49 +10,47 @@ use Bitrix\Main\Page\Asset;
  */
 final class ReactRenderer extends Singleton implements RendererInterface
 {
-	protected static $instance;
+    private $installed;
 
-	private $installed;
+    public function __construct()
+    {
+        $this->installed = array();
+    }
 
-	public function __construct()
-	{
-		$this->installed = array();
-	}
+    public static function connect(string $relative_dir): void
+    {
+        $instance = self::getInstance();
 
-	public static function connect(string $relative_dir): void
-	{
-		$instance = self::getInstance();
+        global $DOCUMENT_ROOT;
 
-		global $DOCUMENT_ROOT;
+        $els = scandir($DOCUMENT_ROOT . $relative_dir);
 
-		$els = scandir($DOCUMENT_ROOT . $relative_dir);
+        foreach ($els as $file) {
+            $matches = array();
+            preg_match("/^(.*)\.(.*)$/", $file, $matches);
+            [$full, $name, $ext] = $matches;
 
-		foreach ($els as $file) {
-			$matches = array();
-			preg_match("/^(.*)\.(.*)$/", $file, $matches);
-			[$full, $name, $ext] = $matches;
+            if ($instance->installed[$name] === null && $ext === "js") {
+                $instance->installed[$name] = $DOCUMENT_ROOT . $relative_dir . "/" . $file;
 
-			if ($instance->installed[$name] === null && $ext === "js") {
-				$instance->installed[$name] = $DOCUMENT_ROOT . $relative_dir . "/" . $file;
+                $asset_inst = Asset::getInstance();
+                $asset_inst->addString("<script type=\"text/babel\" src=\"$relative_dir/$file\"></script>");
+            }
+        }
+    }
 
-				$asset_inst = Asset::getInstance();
-				$asset_inst->addString("<script type=\"text/babel\" src=\"$relative_dir/$file\"></script>");
-			}
-		}
-	}
+    public static function render(string $name, array $params = []): void
+    {
+        $id = uniqid("react_", false);
 
-	public static function render(string $name, array $params = []): void
-	{
-		$id = uniqid("react_", false);
+        $props = json_encode($params, JSON_THROW_ON_ERROR);
 
-		$props = json_encode($params, JSON_THROW_ON_ERROR);
-
-		echo <<< JS
+        echo <<< JS
 		<div id="$id">
 			<script type="text/babel">
 				ReactDOM.render(<$name {...$props}/>, document.querySelector("#$id"));
 			</script>
 		</div>
 		JS;
-	}
+    }
 }
