46a7fa47a93d13bd1bb8a4aedd7a25bdd7e8a69d
amensum <amensum@ya.ru>
2021-02-01 00:55:49 +0300

    [UP] dev_mode

--- classes/Main.php
+++ classes/Main.php
@@ -13,49 +13,58 @@ use Bitrix\Main\Page\Asset;
  */
 final class Main extends Singleton
 {
-	private $module_absolute_path;
-	private $module_relative_path;
+    private $module_absolute_path;
+    private $module_relative_path;
 
-	public function __construct()
-	{
-		$this->module_absolute_path = str_replace("\\", "/", dirname(__DIR__ . '\\..\\'));
-		$this->module_relative_path = str_replace($_SERVER['DOCUMENT_ROOT'], '', $this->module_absolute_path);
-	}
+    public function __construct()
+    {
+        $this->module_absolute_path = str_replace("\\", "/", dirname(__DIR__ . '\\..\\'));
+        $this->module_relative_path = str_replace($_SERVER['DOCUMENT_ROOT'], '', $this->module_absolute_path);
+    }
 
-	public static function onProlog(): void
-	{
-		$instance = self::getInstance();
+    public static function onProlog(): void
+    {
+        $instance = self::getInstance();
 
-		$include_js = Settings::getSpaceField("UF_INCLUDE_JS", "SYSTEM");
-		$include_css = Settings::getSpaceField("UF_INCLUDE_CSS", "SYSTEM");
-		$include_react = Settings::getSpaceField("UF_INCLUDE_REACT", "SYSTEM");
+        $dev_mode = Settings::getSpaceField("UF_DEV_MODE", "SYSTEM");
+        $include_js = Settings::getSpaceField("UF_INCLUDE_JS", "SYSTEM");
+        $include_css = Settings::getSpaceField("UF_INCLUDE_CSS", "SYSTEM");
+        $include_react = Settings::getSpaceField("UF_INCLUDE_REACT", "SYSTEM");
 
-		if ($include_js) {
-			$asset_inst = Asset::getInstance();
-			$asset_inst->addJs($instance->module_relative_path . '/assets/js/ion.js');
-		}
+        if ($include_js) {
+            $asset_inst = Asset::getInstance();
+            $asset_inst->addJs($instance->module_relative_path . '/assets/js/ion.js');
+        }
 
-		if ($include_css) {
-			$asset_inst = Asset::getInstance();
-			$asset_inst->addCss($instance->module_relative_path . '/assets/css/ion.css');
-		}
+        if ($include_css) {
+            $asset_inst = Asset::getInstance();
+            $asset_inst->addCss($instance->module_relative_path . '/assets/css/ion.css');
+        }
 
-		if ($include_react) {
-			$asset_inst = Asset::getInstance();
-			$asset_inst->addString("<script src=\"https://unpkg.com/babel-standalone@6/babel.min.js\"></script>");
-			$asset_inst->addString("<script src=\"https://unpkg.com/react@17/umd/react.development.js\"></script>");
-			$asset_inst->addString("<script src=\"https://unpkg.com/react-dom@17/umd/react-dom.development.js\"></script>");
-			$asset_inst->addString("<script src=\"https://unpkg.com/react-bootstrap@1.4.0/dist/react-bootstrap.min.js\"></script>");
-		}
-	}
+        if ($include_react) {
+            $asset_inst = Asset::getInstance();
 
-	public static function onEpilog(): void
-	{
-		$instance = self::getInstance();
-	}
+            if ($dev_mode) {
+                $asset_inst->addString("<script src=\"https://unpkg.com/babel-standalone@6/babel.js\"></script>");
+                $asset_inst->addString("<script src=\"https://unpkg.com/react@17/umd/react.development.js\"></script>");
+                $asset_inst->addString("<script src=\"https://unpkg.com/react-dom@17/umd/react-dom.development.js\"></script>");
+                $asset_inst->addString("<script src=\"https://unpkg.com/react-bootstrap@1.4.0/dist/react-bootstrap.js\"></script>");
+            } else {
+                $asset_inst->addString("<script src=\"https://unpkg.com/babel-standalone@6/babel.min.js\"></script>");
+                $asset_inst->addString("<script src=\"https://unpkg.com/react@17/umd/react.production.min.js\"></script>");
+                $asset_inst->addString("<script src=\"https://unpkg.com/react-dom@17/umd/react-dom.production.min.js\"></script>");
+                $asset_inst->addString("<script src=\"https://unpkg.com/react-bootstrap@1.4.0/dist/react-bootstrap.min.js\"></script>");
+            }
+        }
+    }
 
-	public static function onAfterEpilog(): void
-	{
-		$instance = self::getInstance();
-	}
+    public static function onEpilog(): void
+    {
+        $instance = self::getInstance();
+    }
+
+    public static function onAfterEpilog(): void
+    {
+        $instance = self::getInstance();
+    }
 }
